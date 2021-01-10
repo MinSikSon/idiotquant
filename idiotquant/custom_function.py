d0daa3cd4e45e8d102fba8b47f09eaaeaf94c0aa
Kenny Oh <ohdoking@gmail.com>
2021-01-10 10:26:40 +0100

    #3 fixed sample logic

--- idiotquant/custom_function.py
+++ idiotquant/custom_function.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 '''
 	CustomFunction class
-	
+
 	User need to change only this part
 '''
 class CustomFunction:
@@ -12,13 +12,12 @@ class CustomFunction:
 	stockNum = 20;       # 주식 종목 수
 
 	def stockFilter(self, stock):
-		if stock.getPER() == 'None':
+		if stock.getPER() == 'None' or stock.getPER() == '0.0':
 			return False
-		# if int(stock.getClose()) > 200000:
-		# 	return True
-		# return False
+		if float(stock.getPER()) > 3.0:
+			return False
 		return True
 
 	def stockPortfolioBuilder(self, stockList):
-		return sorted(stockList, key=lambda x: float(x.getPER()), reverse=True)
+		return sorted(stockList, key=lambda x: float(x.getPER()), reverse=False)
 		# return None

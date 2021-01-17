597f51cd9055eb0be3f798cf0060b4bbd93e3270
minsikson <sonminsik@sonminsig-ui-MacBook-Pro.local>
2021-01-17 17:56:48 +0900

    #14 krx 데이터를 하나로 합치기 위한 함수 getMarketValue  추가

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from interface.opendart import OpenDart
 from interface.krx import Krx
 from interface.corpcode import CorpCode
+import interface.common as Common
 
 def sample_1():
+    print("#####", sample_1.__name__, "#####")
+
     businessYear = 2020
     businessQuarter = 1
     date = "20201224"
@@ -36,6 +39,8 @@ def sample_1():
     print(mergeDic["삼성전자"])
 
 def sample_2():
+    print("#####", sample_2.__name__, "#####")
+
     corpName = "삼성전자"
     businessYear = 2020
     businessQuarter = 1
@@ -47,19 +52,49 @@ def sample_2():
     print(financialInfoAll[corpName])
 
 def sample_3():
+    print("#####", sample_3.__name__, "#####")
+
     corpName = "삼성전자"
-    date = "20201224"
+    # date = "20201101"
+    date = "20200831"
 
     stKrx = Krx()
-    resFundamental = stKrx.getMarketFundamentalByTicker(date)
-    print(resFundamental[corpName])
-
     resOhlcv = stKrx.getMarketOhlcvByTicker(date)
     print(resOhlcv[corpName])
 
+    resFundamental = stKrx.getMarketFundamentalByTicker(date)
+    print(resFundamental[corpName])
+
+def sample_4():
+    print("#####", sample_4.__name__, "#####")
+
+    aCorpName = ["삼성전자", "SK하이닉스", "롯데정보통신"]
+    date = "20201224"
+
+    print(aCorpName, date)
+
+    stKrx = Krx()
+    resMarketValue = stKrx.getMarketValue(date)
+    for corpName in aCorpName:
+        print(corpName, resMarketValue["data"][corpName])
+
+def sample_5():
+    print("#####", sample_5.__name__, "#####")
+
+    date = "20201223"
+    # date = "20201224"
+
+    stKrx = Krx()
+    extractPath = "./market_all_" + date + ".json"
+    Common.extractJson(stKrx.getMarketValue(date), extractPath)
+
 if __name__ == "__main__" :
-    sample_1() # NOTE: opendart 및 krx 에서 얻어온 데이터를 하나로 합치는 예.
+    # sample_1() # NOTE: opendart 및 krx 에서 얻어온 데이터를 하나로 합치는 예.
 
-    sample_2() # NOTE: OpenDart 사용 예.
+    # sample_2() # NOTE: OpenDart 사용 예.
 
-    sample_3() # NOTE: Krx 사용 예.
+    # sample_3() # NOTE: Krx 사용 예.
+
+    # sample_4() # NOTE: Krx 에서 얻을 수 있는 모든 data 를 추출
+
+    sample_5() # NOTE: Krx 에서 데이터 얻어서 json file 로 저장

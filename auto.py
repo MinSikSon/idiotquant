76d3f2a0b0347867abc97c9b6187908f30858ae4
minsikson <sonminsik@sonminsig-ui-MacBook-Pro.local>
2021-07-21 01:25:11 +0900

    add schedule module

--- auto.py
+++ auto.py
@@ -3,6 +3,7 @@ import json
 import time
 import main
 import datetime
+import schedule  # pip install schedule
 
 
 def postMarketInfoInRange():
@@ -46,110 +47,37 @@ def postMarketInfoInRange():
     exit()
 
 
-if __name__ == "__main__":
-    # postMarketInfoInRange()
+def postTodayMarketInfo():
     _url = 'http://221.163.190.38:3000/stock/market-info'
     # _url = 'http://localhost:3000/stock/market-info'
     print('_url', _url)
 
     time.strftime('%c', time.localtime(time.time()))
+    _date = time.strftime('%Y%m%d', time.localtime(time.time()))
     _dayOfWeekend = time.strftime('%w', time.localtime(time.time()))
-    print(_dayOfWeekend)
+    print(_date, _dayOfWeekend)
     if _dayOfWeekend is "6" or _dayOfWeekend is "0":
         print('Today is the weekend. (%s)', _dayOfWeekend)
-        exit()
+        return
 
-    _date = time.strftime('%Y%m%d', time.localtime(time.time()))
-
-    # mod
-    # _date = "20161214"
-    # _date = "20170109"
-    # _date = "20170116"
-    # _date = "20170126"
-    # _date = "20170206"
-    # _date = "20170213"
-    # _date = "20170220"
-    # _date = "20170227"
-    # _date = "20170306"
-    # _date = "20170313"
-
-    # _date = "20170320"
-    # _date = "20170327"
-    # _date = "20170403"
-    # _date = "20170410"
-    # _date = "20170417"
-    # _date = "20170424"
-    # _date = "20180201"
-    # _date = "20180720"
-    # _date = "20180721"
-    # _date = "20180722"
-    # _date = "20180723"
-    # _date = "20180724"
-    # _date = "20180725"
-    # _date = "20180726"
-    # _date = "20180727"
-    # _date = "20180728"
-    # _date = "20180729"
-    # _date = "20180730"
-    # _date = "20180731"
-    # _date = "20190201"
-    # _date = "20190213"
-    # _date = "20190422"
-    # _date = "20190713"
-    # _date = "20190714"
-    # _date = "20190715"
-    # _date = "20190716"
-    # _date = "20190717"
-    # _date = "20190718"
-    # _date = "20190719"
-    # _date = "20190720"
-    # _date = "20190721"
-    # _date = "20200104"
-    # _date = "20200131"
-    # _date = "20201223"
-    # _date = "20210104"
-    # _date = "20210118"
-    # _date = "20210119"
-    # _date = "20210122"
-    # _date = "20210201"
-    # _date = "20210302"
-    # _date = "20210303"
-    # _date = "20210413"
-    # _date = "20210414"
-    # _date = "20210415"
-    # _date = "20210416"
-    # _date = "20210419"
-    # _date = "20210420"
-    # _date = "20210421"
-    # _date = "20210422"
-    # _date = "20210611"
-    # _date = "20210622"
-    # _date = "20210623"
-    # _date = "20210624"
-    # _date = "20210625"
-    # _date = "20210628"
-    # _date = "20210629"
-    # _date = "%d" % (20210629)
-    # _date = "20210630"
-    # _date = "20210705"
-    # _date = "20210706"
-    # _date = "20210707"
-    # _date = "20210708"
-    # _date = "20210709"
-    # _date = "20210712"
-    # _date = "20210713"
-    # _date = "20210714"
-    # _date = "20210715"
-    # _date = "20210719"
     _date = "20210720"
 
     main.extractMarketInformationAllOnly(_date)
 
     _file_name = 'marketInformation_%s.json' % _date
-    print('_file_name', _file_name)
     _file_path = 'data/%s' % _file_name
-    print('_file_path', _file_path)
+    print('_file_name', _file_name, ', _file_path', _file_path)
     with open(_file_path, 'r', encoding='utf-8') as raw:
         json_data = json.load(raw)
         res = requests.post(_url, json=json_data)
         print(res)
+
+
+if __name__ == "__main__":
+    postTime = '16:00:00'
+    print('[postTodayMarketInfo]', 'postTime: every', postTime)
+    schedule.every().day.at(postTime).do(postTodayMarketInfo)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
